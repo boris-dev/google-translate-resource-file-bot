8b13140150d1e8ac292d6857cd6e5bd1ec8c59f6
bshestakov <bshestakov@at-consulting.com>
2018-02-02 18:43:13 +0300

    try again 2

--- src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
+++ src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
@@ -22,24 +22,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class HelloworldApplication {
-  @RequestMapping("/")
-  public String home() {
-    return "I'm future bot1!";
-  }
-
-  /**
-   * (Optional) App Engine health check endpoint mapping.
-   * @see <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking"></a>
-   * If your app does not handle health checks, a HTTP 404 response is interpreted
-   *     as a successful reply.
-   */
-  @RequestMapping("/_ah/health")
-  public String healthy() {
-    // Message body required though ignored
-    return "Still surviving.";
-  }
+//  @RequestMapping("/")
+//  public String home() {
+//    return "I'm future bot1!";
+//  }
+//
+//  /**
+//   * (Optional) App Engine health check endpoint mapping.
+//   * @see <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking"></a>
+//   * If your app does not handle health checks, a HTTP 404 response is interpreted
+//   *     as a successful reply.
+//   */
+//  @RequestMapping("/_ah/health")
+//  public String healthy() {
+//    // Message body required though ignored
+//    return "Still surviving.";
+//  }
 
   public static void main(String[] args) {
     System.out.println("HELLO!");
