7cfdccbc35199b0628593c3c11541267105daf7f
bshestakov <bshestakov@at-consulting.com>
2018-02-02 18:59:06 +0300

    убрал лишнее

--- src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
+++ src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
@@ -16,26 +16,7 @@
 
 package com.example.java.gettingstarted;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class HelloworldApplication {
-//  @RequestMapping("/")
-//  public String home() {
-//    return "I'm future bot1!";
-//  }
-//
-//  /**
-//   * (Optional) App Engine health check endpoint mapping.
-//   * @see <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking"></a>
-//   * If your app does not handle health checks, a HTTP 404 response is interpreted
-//   *     as a successful reply.
-//   */
-//  @RequestMapping("/_ah/health")
-//  public String healthy() {
-//    // Message body required though ignored
-//    return "Still surviving.";
-//  }
 
     public static void main(String[] args) {
         System.out.println("SLDKFJLSDFKJ");
