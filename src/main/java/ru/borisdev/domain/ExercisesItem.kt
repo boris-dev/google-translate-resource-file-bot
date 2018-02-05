04a48bf54774a61ab7daf94ace579fe0e67eabb0
vkoba <ricoshet@list.ru>
2018-02-05 23:06:37 +0300

    - баг

--- src/main/java/ru/borisdev/domain/ExercisesItem.kt
+++ src/main/java/ru/borisdev/domain/ExercisesItem.kt
@@ -1,8 +1,8 @@
 package ru.liga.domain
 
 data class ExercisesItem(val textByLang: TextByLang,
-                         val youtobeLink: String?,
+                         val youtobeLink: String? = null,
                          val midiBase64: String = "",
                          val guid: String = "",
                          val type: String = "",
-                         val tonika: String?)
+                         val tonika: String? = null)
