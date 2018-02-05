afc06a34659cc41947c52f91b81f2066a7d3ee48
bshestakov <bshestakov@at-consulting.com>
2018-02-05 22:30:27 +0300

    midiBase64

--- src/main/java/ru/borisdev/domain/ExercisesItem.kt
+++ src/main/java/ru/borisdev/domain/ExercisesItem.kt
@@ -2,7 +2,7 @@ package ru.liga.domain
 
 data class ExercisesItem(val textByLang: TextByLang,
                          val youtobeLink: String = "",
-                         val midiBase: String = "",
+                         val midiBase64: String = "",
                          val guid: String = "",
                          val type: String = "",
                          val tonika: String = "")
