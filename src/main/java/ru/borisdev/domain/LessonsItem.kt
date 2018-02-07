6ce7a10d5e6183ffd3dae6625c007e9358f67e66
bshestakov <bshestakov@at-consulting.com>
2018-02-07 23:07:52 +0300

    null

--- src/main/java/ru/borisdev/domain/LessonsItem.kt
+++ src/main/java/ru/borisdev/domain/LessonsItem.kt
@@ -1,6 +1,6 @@
 package ru.liga.domain
 
-data class LessonsItem(val exercises: List<ExercisesItem>,
+data class LessonsItem(val exercises: List<ExercisesItem>? = null,
                        val comingSoon: Boolean = false,
                        val name: String = "",
                        val guid: String = "",
